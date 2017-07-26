832ef60c90ec828466c2aeb400e8b6e460744036
prabin tp <prabintp@gmail.com>
2017-07-26 18:24:37 +0530

    integrated with react

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+/*
+    ./client/components/App.jsx
+*/
+
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>something went success</h1>
+      </div>);
+  }
+}
--- client/index.js
+++ client/index.js
@@ -2,4 +2,11 @@
     ./client/index.js
     which is the webpack entry file
 */
-console.log('Hey guys and ladies!!')
+/*
+    ./client/index.js
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
